e2566a265b391ae87d8ec641c27e5a19160b278c
chockwadi <chockwadi.shoaib@gmail.com>
2020-08-16 01:31:57 +0500

    first commit

---
+++ precache-manifest.469553cd83923c9051d8a765bcbaa466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31b76554243b7e805a9b71e58aba2aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3e449578183fbb3ce7",
+    "url": "/static/css/main.5f2a2c85.chunk.css"
+  },
+  {
+    "revision": "f3b6fd4a02c1b5013ee1",
+    "url": "/static/js/2.a4462e5c.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.a4462e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3e449578183fbb3ce7",
+    "url": "/static/js/main.e1c2d291.chunk.js"
+  },
+  {
+    "revision": "e17c5a3b3eb3cdaf53ef",
+    "url": "/static/js/runtime-main.dc40691d.js"
+  }
+]);
